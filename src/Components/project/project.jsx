3c0cf85c4dde577d60154928907f6115f6224e20
Sebastian Deluca <42455619+sebastiandeluca@users.noreply.github.com>
2024-01-18 17:27:03 -0500

    changed projects page and setup contact

--- src/Components/project/project.jsx
+++ src/Components/project/project.jsx
@@ -3,14 +3,16 @@ import '../../App.scss';
 import React from 'react';
 
 const Project = (props) => {
+    // NEED TO FIX ON PORTRAIT-MODE IPAD
+    // 
     return (
-        <div className='project-container'>
+        <div className='project-container animateda' style={{'--order': props.order}}>
             
             <img className='proj-logo' src={props.data.title.logo} alt={`${props.data.title.title} logo`}></img>
             <h1>{props.data.title.title}</h1>
             <p className='date'>{props.data.date}</p>
             <p className='desc'>{props.data.desc}</p>
-            <span className="tag" style={{ background: '#CFBF2D' }}>
+            <span className="tag" style={{ background: '#cf392e' }}>
                 {props.data.category.tag}
             </span>
         </div>
